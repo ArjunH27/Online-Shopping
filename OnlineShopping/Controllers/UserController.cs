e6dd80c9a61318157795b310fb37ed0d173bf2ae
ArjunH27 <arjunhariharasubramani27@gmail.com>
2017-10-05 14:17:37 +0530

    login and registration

---
+++ OnlineShopping/Controllers/UserController.cs
@@ -0,0 +1,114 @@
+﻿using OnlineShopping.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineShopping.Controllers
+{
+    public class UserController : Controller
+    {
+        ShoppingCartDbEntities db = new ShoppingCartDbEntities();
+        // GET: User
+        public ActionResult create()
+        {
+            List<Role_Table> role = new List<Role_Table>();
+            role = db.Role_Table.ToList();
+            var rolelist = new List<SelectListItem>();
+            foreach(var item in role)
+            { rolelist.Add(new SelectListItem
+                {
+                    Text = item.RoleName.ToString(),
+                    Value = item.RoleId.ToString()
+                });
+            }
+            ViewBag.rolename = rolelist;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult create(User_Table obj,string x)
+        {
+            if(ModelState.IsValid)
+            { 
+            obj.UserCreatedDate = System.DateTime.Now;
+            obj.UserUpdatedDate = System.DateTime.Now;
+            if(obj.Roleid==1|| obj.Roleid == 2|| obj.Roleid == 3)
+            {
+                obj.UserIsDeleted = 1;
+            }
+            else 
+            {
+                obj.UserCreatedBy = obj.UserName;
+                obj.UserIsDeleted = 0;
+            }
+            db.User_Table.Add(obj);
+            db.SaveChanges();
+            return RedirectToAction("login");
+            }
+            else
+            {
+                return RedirectToAction("errror");
+            }
+        }
+
+
+        [HttpGet]
+        public ActionResult login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult login(string user,string password)
+        {
+            User_Table obj = db.User_Table.Where(x => x.UserName == user).FirstOrDefault();
+            if(obj!=null)
+            {
+                if(obj.Password==password)
+                {
+                    if(obj.Roleid==1 && obj.UserIsDeleted==0)
+                    {
+                        return RedirectToAction("Index", "Admin");
+                    }
+                    else if(obj.Roleid == 2 && obj.UserIsDeleted == 0)
+                    {
+                        return RedirectToAction("Index", "Seller");
+                    }
+                    else if (obj.Roleid == 3 && obj.UserIsDeleted == 0)
+                    {
+                        return RedirectToAction("Index", "Service");
+                    }
+                    else if(obj.Roleid==4)
+                    {
+                        return RedirectToAction("Index", "Buyer");
+                    }
+                    else
+                    {
+                        return RedirectToAction("errror");
+                    }
+
+                }
+                else
+                {
+                    return View();
+                   
+                   
+                }
+            }
+            else
+            {
+                eturn View();
+            }
+            return View();
+        }
+
+
+        [HttpGet]
+        public ActionResult errror()
+        {
+            return View();
+        }
+    }
+}
